14fbfe7f5ee716fcd4a9acb2872384d598dc8d77
mammad2c <m_dd@rogers.com>
2017-04-10 03:15:00 +0430

    Style nav bar

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -1,21 +1,50 @@
 var React = require('react');
 var {NavLink} = require('react-router-dom');
 
-var Nav = (props) => {
-  let activeStyles = {
-    fontWeight: 'bold',
-    color: 'green'
+var Nav = React.createClass({
+  onSearch: function(e) {
+    e.preventDefault();
+    alert("not ready yet");
+  },
+  render: function() {
+    let activeStyles = {
+      fontWeight: 'bold'
+    }
+    return (
+      <div className="top-bar">
+        <div className="top-bar-left">
+          <ul className="menu">
+            <li className="menu-text">
+              React Weather App
+            </li>
+            <li>
+              <NavLink exact to="/" activeClassName="active" replace activeStyle={activeStyles}>Get Weather
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/about" replace activeClassName="active" activeStyle={activeStyles}>
+                About
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/examples" activeClassName="active" replace activeStyle={activeStyles}>Examples</NavLink>
+            </li>
+          </ul>
+        </div>
+        <div className="top-bar-right">
+          <form onSubmit={this.onSearch}>
+            <ul className="menu">
+              <li>
+                <input type="search" placeholder="Search weather"/>
+              </li>
+              <li>
+                <input type="submit" className="button" value="Get Weather"/>
+              </li>
+            </ul>
+          </form>
+        </div>
+      </div>
+    )
   }
-  return (
-    <div>
-      <h2>Nav Component Is Rendering</h2>
-      <NavLink exact to="/" replace activeStyle={activeStyles}>Get Weather
-      </NavLink>
-      <NavLink to="/about" replace activeStyle={activeStyles}>
-        About
-      </NavLink>
-      <NavLink to="/examples" replace activeStyle={activeStyles}>Examples</NavLink>
-    </div>
-  )
-};
+})
 module.exports = Nav;
